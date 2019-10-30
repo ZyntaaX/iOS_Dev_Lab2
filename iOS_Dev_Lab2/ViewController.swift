59ea26b868b1c057d84dd08330ed8c215a565b86
Rasmus <rasmus.svahnberg97>
2019-10-30 10:16:32 +0000

    Initial Commit

---
+++ iOS_Dev_Lab2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Dev_Lab2
+//
+//  Created by user162693 on 2019-10-30.
+//  Copyright © 2019 RasmusSvanberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
