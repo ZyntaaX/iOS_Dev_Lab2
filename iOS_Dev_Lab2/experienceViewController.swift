79030b8dd528fe227d9cde1208f855bb7bdc8c6d
Rasmus <rasmus.svahnberg97>
2019-10-31 10:37:25 +0000

    Experience table done. Need work on navigation menu in Landscape mode.

---
+++ iOS_Dev_Lab2/experienceViewController.swift
@@ -0,0 +1,28 @@
+//
+//  experienceViewController.swift
+//  iOS_Dev_Lab2
+//
+//  Created by user162693 on 2019-10-30.
+//  Copyright © 2019 RasmusSvanberg. All rights reserved.
+//
+
+import UIKit
+
+class experienceViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
