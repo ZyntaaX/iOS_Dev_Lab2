09cdd88681152080df4e3627d1456e7d4fe9d145
Rasmus <rasmus.svahnberg97>
2019-11-04 10:07:28 +0000

    test

---
+++ iOS_Dev_Lab2/DetailedExperienceViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailedExperienceViewController.swift
+//  iOS_Dev_Lab2
+//
+//  Created by user162693 on 2019-11-04.
+//  Copyright © 2019 RasmusSvanberg. All rights reserved.
+//
+
+import UIKit
+
+class DetailedExperienceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ iOS_Dev_Lab2/experienceCell.swift
@@ -0,0 +1,16 @@
+//
+//  experienceCell.swift
+//  iOS_Dev_Lab2
+//
+//  Created by user162693 on 2019-11-04.
+//  Copyright © 2019 RasmusSvanberg. All rights reserved.
+//
+
+import UIKit
+
+class experienceCell: UITableViewCell {
+
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var date: UILabel!
+}
